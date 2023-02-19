78244ebb279aff4ef36be64be3818b2c75f0baca
cagdas_karabulut <ckarabulut@adersystems.com>
2023-02-19 17:36:36 +0300

    first commit

---
+++ src/main/java/com/karabulut/arabulucubackend/ArabulucuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.karabulut.arabulucubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArabulucuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArabulucuBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karabulut/arabulucubackend/ArabulucuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karabulut.arabulucubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArabulucuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
